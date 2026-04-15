0112a514b72f145c1c06773f802a92fec8562316
namikawa <namikawa@accorder.co.jp>
2026-04-15 10:01:11 +0900

    faf

--- src/texture.js
+++ src/texture.js
@@ -1,11 +1,11 @@
 import * as THREE from "three";
 
 const images = [
-  "img/aniversary.webp",
-  "img/arcraft.webp",
-  "img/nenga-2024.webp",
-  "img/nenga-2026.webp",
-  "img/portfolio-2024.webp",
+  "/img/aniversary.webp",
+  "/img/arcraft.webp",
+  "/img/nenga-2024.webp",
+  "/img/nenga-2026.webp",
+  "/img/portfolio-2024.webp",
 ];
 
 const loader = new THREE.TextureLoader();
@@ -14,6 +14,5 @@ export async function createTexture(i) {
   const path = images[i % images.length];
   const tex = await loader.loadAsync(path);
   tex.colorSpace = THREE.SRGBColorSpace;
-
   return tex;
 }
